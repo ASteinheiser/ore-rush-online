baf9e599f6b105ac9c9f2086723bb04b1d4a8b79
ASteinheiser <me@iamandrew.io>
2026-02-21 01:32:45 -0700

    update ecosystem max memory restart guard to 600mb now that droplet is upgraded

--- apps/game-api/ecosystem.config.cjs
+++ apps/game-api/ecosystem.config.cjs
@@ -11,7 +11,7 @@ module.exports = {
       env_file: '.env',
       instances: 1,
       exec_mode: 'fork',
-      max_memory_restart: '350M',
+      max_memory_restart: '600M',
       out_file: '/home/deployer/logs/game-api-out.log',
       error_file: '/home/deployer/logs/game-api-err.log',
     },
