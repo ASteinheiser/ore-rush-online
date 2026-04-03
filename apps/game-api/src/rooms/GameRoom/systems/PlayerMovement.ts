f5ca55024b5c103820d179b434102047063a1436
ASteinheiser <me@iamandrew.io>
2026-04-03 02:15:31 -0700

    properly spawn player at top of map in empty rows

--- apps/game-api/src/rooms/GameRoom/systems/PlayerMovement.ts
+++ apps/game-api/src/rooms/GameRoom/systems/PlayerMovement.ts
@@ -1,4 +1,12 @@
-import { MAP_SIZE, PLAYER_SIZE, calculateMovement, type InputPayload, type Player } from '@repo/core-game';
+import {
+  MAP_SIZE,
+  EMPTY_MAP_ROWS,
+  BLOCK_SIZE,
+  PLAYER_SIZE,
+  calculateMovement,
+  type InputPayload,
+  type Player,
+} from '@repo/core-game';
 import { logger } from '../../../logger';
 import type { GameRoom } from '../index';
 
@@ -17,24 +25,34 @@ export class PlayerMovement {
       // existing players already have a position, so we don't need to spawn them
     } else {
       player.x = Math.random() * MAP_SIZE.width;
-      player.y = Math.random() * MAP_SIZE.height;
+      // ensure the player spawns in the empty map rows
+      player.y =
+        PLAYER_SIZE.height / 2 + Math.random() * (EMPTY_MAP_ROWS * BLOCK_SIZE.height - PLAYER_SIZE.height);
     }
 
     this.room.state.players.set(clientId, player);
   }
 
   public handleInput(player: Player, input: InputPayload) {
-    if (input.left) player.isFacingRight = false;
-    else if (input.right) player.isFacingRight = true;
+    const blocks = this.room.blockMap.getNearbyBlocks(player).map(({ x, y }) => ({ x, y }));
 
     const newPosition = calculateMovement({
       x: player.x,
       y: player.y,
-      ...PLAYER_SIZE,
-      ...input,
+      velocityY: player.velocityY,
+      width: PLAYER_SIZE.width,
+      height: PLAYER_SIZE.height,
+      left: input.left,
+      right: input.right,
+      up: input.up,
+      blocks,
     });
 
     player.x = newPosition.x;
     player.y = newPosition.y;
+    player.velocityY = newPosition.velocityY;
+    player.isGrounded = newPosition.isGrounded;
+    player.isTouchingBlockLeft = newPosition.isTouchingBlockLeft;
+    player.isTouchingBlockRight = newPosition.isTouchingBlockRight;
   }
 }
