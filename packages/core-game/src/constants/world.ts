5d0019bff3f17a16fc0de2f2d7e18d87979a09bd
ASteinheiser <me@iamandrew.io>
2026-04-09 00:55:06 -0700

    adjust constants for map cols/rows (more simple)

--- packages/core-game/src/constants/world.ts
+++ packages/core-game/src/constants/world.ts
@@ -3,17 +3,17 @@ import { BLOCK_SIZE } from './block';
 /** How long each tick takes in ms (64fps = 15.625ms) */
 export const FIXED_TIME_STEP = 1000 / 64;
 
-/** The size of the map in pixels */
-export const MAP_SIZE = {
-  width: BLOCK_SIZE.width * 64,
-  height: BLOCK_SIZE.height * 64,
-} as const;
-
 /** The size of the map in grid columns and rows */
 export const MAP_GRID_SIZE = {
-  cols: Math.ceil(MAP_SIZE.width / BLOCK_SIZE.width),
-  rows: Math.ceil(MAP_SIZE.height / BLOCK_SIZE.height),
-};
+  cols: 64,
+  rows: 64,
+} as const;
+
+/** The size of the map in pixels */
+export const MAP_SIZE = {
+  width: BLOCK_SIZE.width * MAP_GRID_SIZE.cols,
+  height: BLOCK_SIZE.height * MAP_GRID_SIZE.rows,
+} as const;
 
 /** The number of rows at the top of the map that are empty (used as a spawn area) */
 export const EMPTY_MAP_ROWS = 2;
