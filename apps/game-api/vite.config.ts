b1b8c5dabf7b8c7cf49c454d7d6ad1445c6d20f8
ASteinheiser <me@iamandrew.io>
2026-04-22 14:16:32 -0700

    mostly working local dev server (needed patch to work + builds still broken)

--- apps/game-api/vite.config.ts
+++ apps/game-api/vite.config.ts
@@ -1,23 +1,40 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
+import { colyseus } from 'colyseus/vite';
 import { builtinModules } from 'module';
 
-export default defineConfig({
-  build: {
-    outDir: 'dist',
-    ssr: 'src/index.ts',
-    target: 'node22',
-    sourcemap: true,
-    rolldownOptions: {
-      external: [
-        ...builtinModules,
-        ...builtinModules.map((m) => `node:${m}`),
-        '@pm2/io',
-        'bufferutil',
-        'utf-8-validate',
-      ],
+export default defineConfig((configEnv) => {
+  const env = loadEnv(configEnv.mode, process.cwd(), '');
+  const PORT = Number(env.PORT);
+  if (isNaN(PORT)) throw new Error('PORT must be a number');
+  if (PORT < 1024 || PORT > 49151) throw new Error('PORT must be between 1024 and 49151');
+
+  return {
+    server: {
+      port: PORT,
     },
-  },
-  ssr: {
-    noExternal: true,
-  },
+    plugins: [colyseus({ serverEntry: '/src/index.ts' })],
+    builder: {
+      async buildApp(builder) {
+        await builder.build(builder.environments.colyseus);
+      },
+    },
+    build: {
+      outDir: 'dist',
+      ssr: 'src/index.ts',
+      target: 'node22',
+      sourcemap: true,
+      rolldownOptions: {
+        external: [
+          ...builtinModules,
+          ...builtinModules.map((m) => `node:${m}`),
+          '@pm2/io',
+          'bufferutil',
+          'utf-8-validate',
+        ],
+      },
+    },
+    ssr: {
+      noExternal: true,
+    },
+  };
 });
