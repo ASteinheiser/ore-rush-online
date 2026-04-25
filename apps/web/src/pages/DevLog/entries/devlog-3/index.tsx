ab526ff09c72e64065dc1c0c50b67b6789090d2a
ASteinheiser <me@iamandrew.io>
2026-04-25 13:02:43 -0700

    update v0.0.5 blog post

--- apps/web/src/pages/DevLog/entries/devlog-3/index.tsx
+++ apps/web/src/pages/DevLog/entries/devlog-3/index.tsx
@@ -6,27 +6,29 @@ export const DevLogEntry3 = () => {
     <DevLogEntry id={3} title="Fuel + Capacity Systems" date="April 25, 2026" author="Andrew Steinheiser">
       <p>
         With the <code>v0.0.5</code> update, I added a simple fuel system and a max capacity to the
-        player&apos;s inventory. The goal is to complete simple vertical slices until the basic game loop is
+        player&apos;s inventory. The goal is to deliver simple vertical slices until a basic game loop is
         complete, which is why these are both very primitive systems at the moment. However, they do stop the
-        player from moving or obtaining ore under the proper conditions.
-      </p>
-
-      <p>
-        I also updated the server logic for input handling, which greatly improves security and consistency
-        across clients. Additionally, I added a small UI improvement: the ability to see the other
-        player&apos;s &quot;signals&quot; (if they&apos;re present in the room)
+        player from moving or obtaining ore under the proper conditions. In addition to the new fuel/weight
+        UI, I added the ability to see the other player&apos;s &quot;signals&quot; (if they&apos;re present in
+        the room).
       </p>
 
       <img src={v005Screenshot} alt="v0.0.5 Screenshot" />
 
       <p>
+        I also updated the server logic for input handling, which greatly improves security and consistency
+        across clients. This change decouples the game simulation (server-side) from the rate of input
+        messages, meaning all players will be processed at the same rate.
+      </p>
+
+      <p>
         After load-testing this version, I discovered that the server logic will need optimizations to
         continue to support the &quot;200 players at once&quot; baseline (which is what I&apos;ve been aiming
-        for). I think this is largely due to the fact that previous input handling logic on the server would
-        process players at the speed of their messages. Which for the load test, is slower than the
-        game&apos;s fixed tick rate. Bots send a message when they receive an update (<code>patchRate</code>{' '}
-        is slower than <code>fixedTick</code>). I believe this was causing previous load tests to consume less
-        CPU/memory.
+        for). I think this is largely due to previous input handling logic that would process players at the
+        speed of their messages. For the load test, this is slower than the game&apos;s fixed tick rate
+        because load test bots send a message when they receive an update (server <code>patchRate</code> is
+        slower than <code>fixedTick</code>). I believe this was causing previous load tests to consume less
+        CPU (false positive).
       </p>
 
       <p>I&apos;m stoked to optimize this a bit and flesh out some more minimal slices of the game loop!</p>
