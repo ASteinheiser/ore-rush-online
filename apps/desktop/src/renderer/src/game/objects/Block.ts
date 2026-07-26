762083dfc8d020134e65ac50033e682c91aa37ed
ASteinheiser <me@iamandrew.io>
2026-07-25 22:10:40 -0700

    refactor Block object internal variables

--- apps/desktop/src/renderer/src/game/objects/Block.ts
+++ apps/desktop/src/renderer/src/game/objects/Block.ts
@@ -17,8 +17,6 @@ const seeded = (seed: number): number => {
 };
 
 export class Block {
-  private currentHp: number;
-  private currentMaxHp: number;
   private hitbox: Phaser.GameObjects.Rectangle;
   private ore: Phaser.GameObjects.Graphics;
   private cracks: Phaser.GameObjects.Graphics;
@@ -30,11 +28,9 @@ export class Block {
     public readonly x: number,
     public readonly y: number,
     type: BLOCK_TYPE,
-    hp: number,
-    maxHp: number
+    public hp: number,
+    private maxHp: number
   ) {
-    this.currentHp = hp;
-    this.currentMaxHp = maxHp;
     this.hitbox = scene.add.rectangle(this.x, this.y, BLOCK_SIZE.width, BLOCK_SIZE.height);
     this.ore = scene.add.graphics().setDepth(1);
     this.cracks = scene.add.graphics().setDepth(99);
@@ -49,20 +45,16 @@ export class Block {
     this.hitbox.destroy();
   }
 
-  get hp() {
-    return this.currentHp;
-  }
-
   public update(hp: number, maxHp: number, type: BLOCK_TYPE) {
-    this.currentHp = hp;
-    this.currentMaxHp = maxHp;
+    this.hp = hp;
+    this.maxHp = maxHp;
     this.setColor(type);
     this.drawCracks(hp, maxHp);
   }
 
   public updateHp(hp: number) {
-    this.currentHp = hp;
-    this.drawCracks(hp, this.currentMaxHp);
+    this.hp = hp;
+    this.drawCracks(hp, this.maxHp);
   }
 
   private setColor(type: BLOCK_TYPE) {
