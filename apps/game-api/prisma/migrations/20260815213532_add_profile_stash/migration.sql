2f634ce13aee941e9ee80a6a41fb3f8feeb9f7b8
ASteinheiser <me@iamandrew.io>
2026-08-15 20:52:04 -0700

    use composite key for stash items and remove duplicate unique index on primary key (profile)

--- apps/game-api/prisma/migrations/20260815213532_add_profile_stash/migration.sql
+++ apps/game-api/prisma/migrations/20260815213532_add_profile_stash/migration.sql
@@ -1,17 +1,16 @@
+-- DropIndex
+DROP INDEX "Profile_userId_key";
+
 -- CreateTable
 CREATE TABLE "Item" (
     "id" TEXT NOT NULL,
     "profileId" TEXT NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
-    "itemId" TEXT NOT NULL,
     "quantity" INTEGER NOT NULL,
 
-    CONSTRAINT "Item_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "Item_pkey" PRIMARY KEY ("profileId","id")
 );
-
--- CreateIndex
-CREATE UNIQUE INDEX "Item_id_key" ON "Item"("id");
 
 -- AddForeignKey
 ALTER TABLE "Item" ADD CONSTRAINT "Item_profileId_fkey" FOREIGN KEY ("profileId") REFERENCES "Profile"("userId") ON DELETE RESTRICT ON UPDATE CASCADE;
