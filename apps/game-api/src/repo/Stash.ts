8d2799cd8ac5c78388e611a88349d543ab3fa910
ASteinheiser <me@iamandrew.io>
2026-08-16 11:25:48 -0700

    create repo layer for storeItemInStash

--- apps/game-api/src/repo/Stash.ts
+++ apps/game-api/src/repo/Stash.ts
@@ -1,4 +1,4 @@
-import type { PrismaClient } from './prisma-client/client';
+import type { PrismaClient, Item } from './prisma-client/client';
 
 export class StashRepository {
   private prisma: PrismaClient;
@@ -11,6 +11,25 @@ export class StashRepository {
     return this.prisma.profile.findUnique({
       where: { userId },
       include: { stash: true },
+    });
+  }
+
+  async storeItemInStash(item: Omit<Item, 'createdAt' | 'updatedAt'>) {
+    return this.prisma.item.upsert({
+      where: {
+        profileId_id: {
+          profileId: item.profileId,
+          id: item.id,
+        },
+      },
+      create: {
+        profileId: item.profileId,
+        id: item.id,
+        quantity: item.quantity,
+      },
+      update: {
+        quantity: { increment: item.quantity },
+      },
     });
   }
 }
