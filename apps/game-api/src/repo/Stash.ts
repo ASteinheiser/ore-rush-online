325578c4b6b7beda3a2f4c75613f1f6d0c72a772
ASteinheiser <me@iamandrew.io>
2026-08-17 23:24:55 -0700

    create repo layer for removeItemFromStash

--- apps/game-api/src/repo/Stash.ts
+++ apps/game-api/src/repo/Stash.ts
@@ -1,5 +1,7 @@
 import type { PrismaClient, Item } from './prisma-client/client';
 
+type ItemWithoutTimestamps = Omit<Item, 'createdAt' | 'updatedAt'>;
+
 export class StashRepository {
   private prisma: PrismaClient;
 
@@ -14,7 +16,7 @@ export class StashRepository {
     });
   }
 
-  async storeItemInStash(item: Omit<Item, 'createdAt' | 'updatedAt'>) {
+  async storeItemInStash(item: ItemWithoutTimestamps) {
     return this.prisma.item.upsert({
       where: {
         profileId_id: {
@@ -31,5 +33,36 @@ export class StashRepository {
         quantity: { increment: item.quantity },
       },
     });
+  }
+
+  async removeItemFromStash(item: ItemWithoutTimestamps) {
+    const [, , remaining] = await this.prisma.$transaction([
+      this.prisma.item.updateMany({
+        where: {
+          profileId: item.profileId,
+          id: item.id,
+        },
+        data: {
+          quantity: { decrement: item.quantity },
+        },
+      }),
+      this.prisma.item.deleteMany({
+        where: {
+          profileId: item.profileId,
+          id: item.id,
+          quantity: { lte: 0 },
+        },
+      }),
+      this.prisma.item.findUnique({
+        where: {
+          profileId_id: {
+            profileId: item.profileId,
+            id: item.id,
+          },
+        },
+      }),
+    ]);
+
+    return remaining;
   }
 }
