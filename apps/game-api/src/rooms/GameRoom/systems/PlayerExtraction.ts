b24190651b877ba89223fdfead2b15a0fc1224a6
ASteinheiser <me@iamandrew.io>
2026-09-06 15:14:29 -0700

    improved handleDeath

--- apps/game-api/src/rooms/GameRoom/systems/PlayerExtraction.ts
+++ apps/game-api/src/rooms/GameRoom/systems/PlayerExtraction.ts
@@ -14,7 +14,7 @@ export class PlayerExtraction {
   /**
    * Called when a player requests to extract (ex: a new WS_EVENT.EXTRACT message).
    * Should validate the player is actually in the extraction zone first.
-   * If successful, then persist the player's inventory.
+   * If successful, then persist the player's inventory and remove them from the map.
    */
   public handleExtractRequest(client: Client) {
     const player = this.room.state.players.get(client.sessionId);
@@ -27,10 +27,12 @@ export class PlayerExtraction {
   }
 
   /**
-   * Called when a player dies (ex: fuel depleted outside of the extraction zone).
+   * Executes when a player dies (ex: fuel depleted outside of the extraction zone).
    * Should discard the player's inventory (do not persist) and remove them from the map.
    */
-  public handleDeath(client: Client) {
-    this.room.auth.kickClient(WS_CODE.DEATH, 'Player has died', client, false);
+  public handleDeath(client: Client, player: Player) {
+    if (player.fuelRemaining <= 0 && !this.isInExtractionZone(player)) {
+      this.room.auth.kickClient(WS_CODE.DEATH, 'Player has died', client, false);
+    }
   }
 }
