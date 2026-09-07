648d2f236d096670660bae3c05fc0e74958dbde8
ASteinheiser <me@iamandrew.io>
2026-09-07 04:23:40 -0700

    improved isInExtract helper

--- packages/core-game/src/is-in-extraction-zone.ts
+++ packages/core-game/src/is-in-extraction-zone.ts
@@ -1,8 +1,18 @@
 import type { Rectangle } from './types';
-import { EMPTY_MAP_ROWS, BLOCK_SIZE } from './constants';
+import { checkAABBCollision } from './check-AABB-collision';
+import { EMPTY_MAP_ROWS, BLOCK_SIZE, MAP_SIZE } from './constants';
 
-/** An entity is consider "in the extraction zone" while their entire reactangle is within the designated area */
+const EXTRACTION_ZONE: Rectangle = {
+  x: MAP_SIZE.width / 2,
+  y: (EMPTY_MAP_ROWS * BLOCK_SIZE.height) / 2,
+  width: MAP_SIZE.width,
+  height: EMPTY_MAP_ROWS * BLOCK_SIZE.height,
+};
+
+/** An entity is considered "in the extraction zone" while their entire rectangle is within the designated area */
 export const isInExtractionZone = (entity: Rectangle) => {
-  const extractionZoneHeight = EMPTY_MAP_ROWS * BLOCK_SIZE.height;
-  return entity.y < extractionZoneHeight;
+  const overlap = checkAABBCollision(entity, EXTRACTION_ZONE);
+  if (!overlap) return false;
+
+  return overlap.overlapX >= entity.width && overlap.overlapY >= entity.height;
 };
