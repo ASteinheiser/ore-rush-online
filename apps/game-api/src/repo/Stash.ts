d7644ae49d33ae15622086e05f90efef509b7402
ASteinheiser <me@iamandrew.io>
2026-09-06 17:43:28 -0700

    create helper for storing multiple items in a transaction

--- apps/game-api/src/repo/Stash.ts
+++ apps/game-api/src/repo/Stash.ts
@@ -9,14 +9,18 @@ export class StashRepository {
     this.prisma = prisma;
   }
 
-  async getStashByUserId(userId: string) {
+  getStashByUserId(userId: string) {
     return this.prisma.profile.findUnique({
       where: { userId },
       include: { stash: true },
     });
   }
 
-  async storeItemInStash(item: ItemWithoutTimestamps) {
+  storeItemInStash(item: ItemWithoutTimestamps) {
+    if (item.quantity <= 0) {
+      throw new Error('Quantity must be greater than 0');
+    }
+
     return this.prisma.item.upsert({
       where: {
         profileId_id: {
@@ -35,12 +39,21 @@ export class StashRepository {
     });
   }
 
+  storeItemsInStash(items: ItemWithoutTimestamps[]) {
+    return this.prisma.$transaction(items.map((item) => this.storeItemInStash(item)));
+  }
+
   async removeItemFromStash(item: ItemWithoutTimestamps) {
+    if (item.quantity <= 0) {
+      throw new Error('Quantity must be greater than 0');
+    }
+
     const [updated] = await this.prisma.$transaction([
       this.prisma.item.updateManyAndReturn({
         where: {
           profileId: item.profileId,
           id: item.id,
+          quantity: { gte: item.quantity },
         },
         data: {
           quantity: { decrement: item.quantity },
