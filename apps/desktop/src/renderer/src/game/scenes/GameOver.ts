ba9d5638fa23f887c4af6c7913261c46853d88c0
ASteinheiser <me@iamandrew.io>
2026-09-07 17:43:16 -0700

    update GameOver screen to show success/failure messages based on outcome

--- apps/desktop/src/renderer/src/game/scenes/GameOver.ts
+++ apps/desktop/src/renderer/src/game/scenes/GameOver.ts
@@ -2,6 +2,7 @@ import * as Phaser from 'phaser';
 import { EventBus, EVENT_BUS } from '../EventBus';
 import { CustomText } from '../objects/CustomText';
 import { ASSET, SCENE } from '../constants';
+import { ORE } from '@repo/core-game';
 import type { InventorySnapshot } from '../systems/UISystem';
 
 export interface GameOverSceneData {
@@ -21,12 +22,16 @@ export class GameOver extends Phaser.Scene {
   }
 
   create({ success, inventory }: GameOverSceneData) {
-    console.log('GameOver constructor', success, inventory);
-    this.cameras.main.setBackgroundColor(0xff0000);
+    this.cameras.main.setBackgroundColor(success ? 0x00ff00 : 0xff0000);
 
     const bg = this.add.image(0, 0, ASSET.BACKGROUND).setAlpha(0.5).setOrigin(0.5);
 
-    const titleText = new CustomText(this, 0, 0, 'Game Over', {
+    const continueText = new CustomText(this, 0, 0, 'Press <SHIFT> to continue', {
+      fontFamily: 'Tiny5',
+      fontSize: 20,
+    }).fadeIn(1500);
+
+    const titleText = new CustomText(this, 0, 0, success ? 'Extraction Successful!' : 'Mission Failed', {
       fontFamily: 'Tiny5',
       fontSize: 64,
       strokeThickness: 8,
@@ -34,10 +39,32 @@ export class GameOver extends Phaser.Scene {
       .setOrigin(0.5)
       .typeWriter(150);
 
-    const continueText = new CustomText(this, 0, 0, 'Press Shift to continue', {
-      fontFamily: 'Tiny5',
-      fontSize: 20,
-    }).fadeIn(1500);
+    const bodyText = new CustomText(
+      this,
+      0,
+      0,
+      success
+        ? 'The following items have been added to your stash:'
+        : 'You lost your haul, along with your ship...',
+      {
+        fontFamily: 'Tiny5',
+        fontSize: 20,
+      }
+    )
+      .setOrigin(0.5)
+      .typeWriter(75);
+
+    const itemTexts: CustomText[] = [];
+    Object.keys(inventory ?? {}).forEach((oreType, index) => {
+      const oreCount = inventory?.[oreType] ?? 0;
+      const oreName = ORE[oreType].name;
+
+      const text = new CustomText(this, 0, 0, `${oreName}: ${oreCount}`, { fontFamily: 'Iceberg' })
+        .setOrigin(0.5)
+        .fadeIn(500, 800 + 500 * (index + 1));
+
+      itemTexts.push(text);
+    });
 
     const layout = () => {
       const { width, height } = this.scale;
@@ -46,6 +73,11 @@ export class GameOver extends Phaser.Scene {
       continueText.setPosition((width - continueText.width) / 2, 20);
 
       titleText.setPosition(width / 2, height / 2 - 100);
+      bodyText.setPosition(width / 2, height / 2 - 30);
+
+      itemTexts.forEach((text, idx) => {
+        text.setPosition(width / 2, height / 2 + 20 + idx * 40);
+      });
     };
 
     layout();
