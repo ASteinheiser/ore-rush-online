2ae72790dc473264a511a30311ac1fa46f1e1f0c
ASteinheiser <me@iamandrew.io>
2026-09-09 03:15:41 -0700

    add title screen after developer logo

--- apps/desktop/src/renderer/src/providers/SplashProvider.tsx
+++ apps/desktop/src/renderer/src/providers/SplashProvider.tsx
@@ -1,30 +1,73 @@
 import { useEffect, useState } from 'react';
 import logo from '../assets/logo.png';
 
-/** This is how long to show the splash screen. Should match 'splash-fade' animation in theme.css (4s) */
-const SPLASH_DURATION = 4 * 1000;
+/** This is how long to show the developer screen. Should match 'developer-fade' animation in theme.css (4s) */
+const DEVELOPER_DURATION = 4 * 1000;
+/** This is how long to show the title screen. Should match 'title-fade' animation in theme.css (6s) */
+const TITLE_DURATION = 6 * 1000;
 
 interface SplashProviderProps {
   children: React.ReactNode;
 }
+
 /**
  * This will play only once when the app is first opened.
  * Good place to put developer and/or publisher logos.
  */
 export const SplashProvider = ({ children }: SplashProviderProps) => {
-  const [isSplashVisible, setIsSplashVisible] = useState(true);
+  const [isDeveloperVisible, setIsDeveloperVisible] = useState(true);
+  const [isTitleVisible, setIsTitleVisible] = useState(false);
 
   useEffect(() => {
-    const timeout = setTimeout(() => setIsSplashVisible(false), SPLASH_DURATION);
-    return () => clearTimeout(timeout);
+    const developerTimeout = setTimeout(() => {
+      setIsDeveloperVisible(false);
+      setIsTitleVisible(true);
+    }, DEVELOPER_DURATION);
+    const titleTimeout = setTimeout(() => setIsTitleVisible(false), DEVELOPER_DURATION + TITLE_DURATION);
+
+    return () => {
+      clearTimeout(developerTimeout);
+      clearTimeout(titleTimeout);
+    };
   }, []);
 
-  if (!isSplashVisible) return children;
+  if (isDeveloperVisible) {
+    return (
+      <div className="fullscreen-center developer-fade" style={{ userSelect: 'none' }}>
+        <h1 className="text-3xl font-label text-muted">developed by</h1>
+        <img src={logo} className="h-40 w-auto mb-10 mt-10" />
+        <h1 className="text-5xl font-pixel text-primary">iamandrew.io</h1>
+      </div>
+    );
+  }
 
-  return (
-    <div className="fullscreen-center splash-fade" style={{ userSelect: 'none' }}>
-      <img src={logo} className="h-40 w-auto mb-15" />
-      <h1 className="text-5xl font-pixel text-primary">iamandrew.io games</h1>
-    </div>
-  );
+  if (isTitleVisible) {
+    return (
+      <div className="fullscreen-center title-fade" style={{ userSelect: 'none' }}>
+        <div className="title-letter-container font-pixel text-primary">
+          <div className="title-letter-group">
+            <span className="title-letter">O</span>
+            <span className="title-subtext text-5xl text-muted">R</span>
+            <span className="title-subtext text-5xl text-muted">E</span>
+          </div>
+          <div className="title-letter-group">
+            <span className="title-letter">R</span>
+            <span className="title-subtext text-5xl text-muted">U</span>
+            <span className="title-subtext text-5xl text-muted">S</span>
+            <span className="title-subtext text-5xl text-muted">H</span>
+          </div>
+          <div className="title-letter-group">
+            <span className="title-letter">O</span>
+            <span className="title-subtext text-5xl text-muted">N</span>
+            <span className="title-subtext text-5xl text-muted">L</span>
+            <span className="title-subtext text-5xl text-muted">I</span>
+            <span className="title-subtext text-5xl text-muted">N</span>
+            <span className="title-subtext text-5xl text-muted">E</span>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  return children;
 };
