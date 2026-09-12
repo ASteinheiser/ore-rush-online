dac61c967c437cbb371fbdc07f63feef46522f6e
ASteinheiser <me@iamandrew.io>
2026-09-12 02:55:15 -0700

    render Ore images on GameOver screen

--- apps/desktop/src/renderer/src/game/scenes/GameOver.ts
+++ apps/desktop/src/renderer/src/game/scenes/GameOver.ts
@@ -2,9 +2,14 @@ import * as Phaser from 'phaser';
 import { ORE } from '@repo/core-game';
 import { EventBus, EVENT_BUS } from '../EventBus';
 import { CustomText } from '../objects/CustomText';
-import { DEPTH, SCENE } from '../constants';
+import { ORE_COLORS, DIRT_COLORS } from '../objects/Block';
+import { ASSET, DEPTH, SCENE } from '../constants';
 import { revealScene, transitionToScene } from '../transitions';
 import type { InventorySnapshot } from '../systems/UISystem';
+
+const ITEM_SLOT_WIDTH = 120;
+const ITEM_NAME_TEXT_GAP = 8;
+const ORE_ICON_SIZE = 64;
 
 export interface GameOverSceneData {
   success: boolean;
@@ -13,7 +18,6 @@ export interface GameOverSceneData {
 
 export class GameOver extends Phaser.Scene {
   private cursorKeys?: Phaser.Types.Input.Keyboard.CursorKeys;
-  private texts: CustomText[] = [];
 
   constructor() {
     super(SCENE.GAME_OVER);
@@ -61,20 +65,46 @@ export class GameOver extends Phaser.Scene {
       .setVisible(!success || heldItems.length > 0)
       .typeWriter(75);
 
-    const itemTexts: CustomText[] = [];
+    const itemRows: Phaser.GameObjects.Container[] = [];
     heldItems.forEach(([oreType, oreCount], index) => {
       const oreName = ORE[oreType].name;
 
-      const text = new CustomText(this, 0, 0, `${oreName}: ${oreCount}`, { fontFamily: 'Iceberg' })
-        .setOrigin(0.5)
+      const oreBg = this.add
+        .image(0, 0, ASSET.ORE_BG)
+        .setDisplaySize(ORE_ICON_SIZE, ORE_ICON_SIZE)
+        .setTint(DIRT_COLORS[2]);
+
+      const oreFg = this.add
+        .image(0, 0, ASSET.ORE_FG)
+        .setDisplaySize(ORE_ICON_SIZE, ORE_ICON_SIZE)
+        .setTint(ORE_COLORS[oreType]);
+
+      // count sits near the bottom-right corner of the ore image
+      const countText = new CustomText(this, ORE_ICON_SIZE / 2, ORE_ICON_SIZE / 2, oreCount, {
+        fontFamily: 'Iceberg',
+        fontSize: 18,
+      }).setOrigin(1);
+
+      // name sits below the ore image/count
+      const nameText = new CustomText(this, 0, ORE_ICON_SIZE / 2 + ITEM_NAME_TEXT_GAP, oreName, {
+        fontFamily: 'Iceberg',
+        fontSize: 20,
+      }).setOrigin(0.5, 0);
+
+      const row = this.add
+        .container(0, 0, [oreBg, oreFg, countText, nameText])
         .setDepth(DEPTH.HUD_FOREGROUND)
-        .fadeIn(500, 800 + 500 * (index + 1));
+        .setAlpha(0);
 
-      itemTexts.push(text);
+      this.tweens.add({
+        targets: row,
+        alpha: 1,
+        duration: 500,
+        delay: 800 + 500 * (index + 1),
+      });
+
+      itemRows.push(row);
     });
-
-    // used for the fade transition back to HomeBase
-    this.texts = [continueText, titleText, bodyText, ...itemTexts];
 
     const layout = () => {
       const { width, height } = this.scale;
@@ -84,8 +114,11 @@ export class GameOver extends Phaser.Scene {
       titleText.setPosition(width / 2, height / 2 - 100);
       bodyText.setPosition(width / 2, height / 2 - 30);
 
-      itemTexts.forEach((text, idx) => {
-        text.setPosition(width / 2, height / 2 + 20 + idx * 40);
+      const totalWidth = itemRows.length * ITEM_SLOT_WIDTH;
+      const startX = width / 2 - totalWidth / 2;
+      itemRows.forEach((row, idx) => {
+        const slotCenterX = startX + idx * ITEM_SLOT_WIDTH + ITEM_SLOT_WIDTH / 2;
+        row.setPosition(slotCenterX, height / 2 + 40);
       });
     };
 
@@ -106,6 +139,6 @@ export class GameOver extends Phaser.Scene {
   }
 
   public changeScene() {
-    transitionToScene(this, SCENE.HOME_BASE, undefined, this.texts);
+    transitionToScene(this, SCENE.HOME_BASE);
   }
 }
