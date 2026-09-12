b33b20190263234917bf2ec19f76bdb57cb9ebc1
ASteinheiser <me@iamandrew.io>
2026-09-12 14:08:51 -0700

    fully styled marketplace modal

--- apps/desktop/src/renderer/src/modals/MarketplaceModal.tsx
+++ apps/desktop/src/renderer/src/modals/MarketplaceModal.tsx
@@ -11,7 +11,7 @@ export const MarketplaceModal = ({ isOpen, onOpenChange }: MarketplaceModalProps
   return (
     <Dialog open={isOpen} onOpenChange={onOpenChange}>
       <DialogContent
-        className="max-w-md w-full"
+        className="max-w-lg w-full"
         onOpenAutoFocus={(e) => e.preventDefault()}
         aria-describedby={undefined}
       >
@@ -19,17 +19,23 @@ export const MarketplaceModal = ({ isOpen, onOpenChange }: MarketplaceModalProps
           <DialogTitle className="pt-2 font-pixel text-4xl text-muted-foreground">Marketplace</DialogTitle>
         </DialogHeader>
 
-        <div className="flex flex-row flex-wrap gap-y-4 gap-x-11 px-2 py-4">
+        <div className="flex flex-row flex-wrap justify-between gap-y-4 px-2 py-4">
           {Object.values(ORE).map((ore) => (
             <div key={ore.id} className="flex flex-col items-center gap-6">
               <OreItem oreId={ore.id} quantity={Infinity} />
 
-              <div className="flex flex-row">
-                <Button variant="outline" size="sm">
-                  Sell
+              <div className="flex flex-row items-center gap-2">
+                <span className="text-md font-pixel text-muted">BUY:</span>
+                <Button size="icon">1</Button>
+                <Button size="icon">10</Button>
+              </div>
+              <div className="flex flex-row items-center gap-2">
+                <span className="text-md font-pixel text-muted">SELL:</span>
+                <Button variant="secondary" size="icon">
+                  1
                 </Button>
-                <Button variant="outline" size="sm">
-                  Buy
+                <Button variant="secondary" size="icon">
+                  10
                 </Button>
               </div>
             </div>
