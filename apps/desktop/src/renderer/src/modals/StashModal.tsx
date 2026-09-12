e4fcba0477510a64322683d7380d9856a7ebdc54
ASteinheiser <me@iamandrew.io>
2026-09-12 03:21:29 -0700

    wireup OreItem to StashModal

--- apps/desktop/src/renderer/src/modals/StashModal.tsx
+++ apps/desktop/src/renderer/src/modals/StashModal.tsx
@@ -1,9 +1,9 @@
 import { Dialog, DialogContent, DialogHeader, DialogTitle } from '@repo/ui';
-import { ORE } from '@repo/core-game';
 import { useSession } from '@repo/client-auth/provider';
 import { gql } from '@apollo/client';
 import { useQuery } from '@apollo/client/react';
 import type { Desktop_GetProfileStashQuery, Desktop_GetProfileStashQueryVariables } from '../graphql';
+import { OreItem } from '../components/OreItem';
 
 const GET_PROFILE_STASH = gql`
   query Desktop_GetProfileStash {
@@ -41,16 +41,9 @@ export const StashModal = ({ isOpen, onOpenChange }: StashModalProps) => {
           <DialogTitle className="pt-2 font-pixel text-4xl text-muted-foreground">Stash</DialogTitle>
         </DialogHeader>
 
-        <div className="flex flex-col gap-6 px-2 pt-6 pb-4">
+        <div className="flex flex-row flex-wrap gap-y-4 gap-x-14 px-2 py-4">
           {stashItems.length > 0 ? (
-            stashItems.map((item) => (
-              <div key={item.id} className="flex flex-row justify-between">
-                <p className="font-title text-xl text-muted-foreground">
-                  {Object.values(ORE).find(({ id }) => id === item.id)?.name}
-                </p>
-                <p className="font-label text-xl text-muted-foreground">{item.quantity}</p>
-              </div>
-            ))
+            stashItems.map((item) => <OreItem key={item.id} oreId={item.id} quantity={item.quantity} />)
           ) : (
             <p className="font-title text-xl text-center text-muted-foreground">Empty...</p>
           )}
